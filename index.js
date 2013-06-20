6814bfebeb5c16adfd74348fa4f3274d0b423ad3
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-06-20 16:27:26 +0300

    added default home page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var createServer = require('http').createServer,
     parse = require('url').parse,
+    createReadStream = require('fs').createReadStream,
     spawn = require('child_process').spawn,
     child = require('event-stream').child;
 
@@ -7,13 +8,16 @@ var port = process.env.PORT || 8000;
 
 function createCmdServer(cmd) {
   return createServer(function(req,res) {
-    var args = parse(req.url,true, true).query.args,
-        proc = child(spawn(cmd, args));
-    req.pipe(proc)
-       .pipe(res);
-    console.log('Executing',cmd,args);
+    if (parse(req.url).method == 'POST') {
+      var args = parse(req.url,true, true).query.args,
+	  proc = child(spawn(cmd, args));
+      req.pipe(proc)
+	 .pipe(res);
+      console.log('Executing',cmd,args);
+    }
+    else // GET
+      createReadStream('./index.html').pipe(res);
   });
-  
 }
 
 createCmdServer('sort').listen(port);
