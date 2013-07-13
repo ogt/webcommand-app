587cf8b69638bcb1f1c505696d06685a66ca1052
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-07-12 18:11:00 -0700

    Added some comments

--- scripts/createapps.sh
+++ scripts/createapps.sh
@@ -1,3 +1,4 @@
+
 for b in "$@"
 do
     git checkout -b $b
@@ -6,3 +7,8 @@ do
     git push -f $b $b:master
 done
 git checkout master
+
+# at this point you will also need to update a couple files in each branch:
+#  - app specific command.js list of commands - assuming that is branch corresponds to a server that 
+#        serves a specific set of commands
+#  - set the branch var in index.html to the branch name so as the fork me points correctly 
