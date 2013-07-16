1df0a021346942fe51dcb2fc93e3d99735fecb7b
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-07-16 12:00:59 -0700

    Made make link only do linking and not deploy as well....

--- scripts/linkapps.sh
+++ scripts/linkapps.sh
@@ -1,11 +1,8 @@
 
 for b in "$@"
 do
-    git checkout -b $b
     git remote add $b git@heroku.com:$b.git
-    git push -f $b $b:master
 done
-git checkout master
 
 # at this point you will also need to update a couple files in each branch:
 #  - app specific command.js list of commands - assuming that is branch corresponds to a server that 
