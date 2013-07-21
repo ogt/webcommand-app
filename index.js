76e72ce8366bee329d04bb8fb40847d9ef27c04a
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-07-21 07:30:25 -0600

    removed debugging stmts

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = require('webcommand-express')(require('./commands'));
 var port = process.env.PORT || 8000;
 
 app.get('/', function(req,res) {
-    console.log(req.host);
     res.sendfile('index.html');
 });
 
